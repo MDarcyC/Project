50946f2f002f09e46133fd81eb63b57f581afa54
MDarcyC <MDarcyconno@gmail.com>
2020-09-06 23:03:10 +1000

    Script for reading through data sets completed.

--- TxtToSQL.py
+++ TxtToSQL.py
@@ -4,16 +4,37 @@ import mysql.connector as mysql
 from pathlib import Path
 from sqlalchemy import create_engine
 
-
-file = open("20200102.txt")
-#print(query)
 engine = create_engine('mysql://example:example@localhost/stockinfo') #Creates database connection
-data = pandas.read_csv(file, sep=",", header=None)  #Reads a given file, separates the data and adds to dataframe
-data.columns = ['ticker', 'date', 'open', 'high', 'low', 'close', 'volume'] #Defines Column Names for Dataframe
-#Need to add function to clean datum pieces
-#Convert datum to appropriate string format
-data.to_sql("stocks", engine, if_exists='append') #to table STOCKS, using current DB connection, append to SQL
-#print(data)
+
+def filetodb(inputFile, directory):
+    inputfile = directory + inputFile
+    file = open(inputfile)
+    data = pandas.read_csv(file, sep=",")  #Reads a given file, separates the data and adds to dataframe
+    data.columns = ['ticker', 'date', 'open', 'high', 'low', 'close', 'volume'] #Defines Column Names for Dataframe
+    dataframetest = data.loc[:]    #Need to add function to clean datum pieces
+    print(dataframetest)
+    file.close()
+
+startpath = 'Data/'
+basepath = os.listdir(startpath) #Displays all subfolders in /Data/
+for x in basepath:
+    i = startpath + x + "/" #Creates a new directory query for next directory readthrough
+    folderfiles = os.listdir(i) # lists all the files in a folder
+    for y in folderfiles:   #for each file in folder files
+            if ".txt" in y: #y is the name of the text file
+                filetodb(y,i)
+
+    #Convert datum to appropriate numerical format
+    #Add table with correct table format
+    #data.to_sql(table, engine, if_exists='append') #to table STOCKS, using current DB connection, append to SQL
+
+
+#def dbadd(filename, date):
+    #Format create table query with proper column FORMATS
+
+
+#def createdatabase(date):
+
 
 file.close()
 x = input()#Pause to test program runs through,i know theres a better way
